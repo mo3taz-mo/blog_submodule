4cc3318bee50243939b3c286d4e4c146bb97fa6c
Moataz Mohamed Osama <Moataz.Osama@andalusia.loc>
2022-05-15 15:01:35 +0200

    add new files to repo

---
+++ index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Blog Page
+    <button class="blog-btn">Create</button>
+  </div>
+</template>
+<style scoped>
+
+.blog-btn {
+  background-color: var(--second);
+  color: var(--black);
+}
+</style>
---
+++ post.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Post Page
+  </div>
+</template>
